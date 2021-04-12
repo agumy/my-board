dfa6962472930116abb44e35c14c848877c46ca8
agumy <nov13sh@gmail.com>
2021-04-12 23:58:38 +0900

    setup eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./styles.css"
 
-const root = document.getElementById("root")!;
+const root = document.getElementById("root")!
 
-ReactDOM.render(<div className="m-10">Hello, world!</div>, root);
+ReactDOM.render(<div className="m-10">Hello, world!</div>, root)
