29abe924a50e870e4b37c3f82f26d1d181edf167
agumy <nov13sh@gmail.com>
2021-04-16 23:34:36 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export const App = (): JSX.Element => {
+export const App: React.VFC = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center p-5"></div>
   )
