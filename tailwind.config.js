d33db4c6478126f386c5120db7bf288c4492ff66
agumy <nov13sh@gmail.com>
2021-04-17 17:20:50 +0900

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./src/index.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: "#F4F5FF",
+      black: "#0C242C",
+      main: "#00AFC7",
+      accent: "#548491",
+      line: "#73AFBF",
+      error: "#CC1F00",
+    },
+    fontFamily: {
+      sans: ['Noto Sans Japanese'],
+    }
   },
   variants: {
     extend: {},
